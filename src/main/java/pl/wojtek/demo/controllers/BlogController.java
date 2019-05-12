244d37105408c3a534d6cbe86c16cc3d2c10793b
Wojciech Cora <wcoraj@gmail.com>
2019-05-12 17:07:52 +0200

    2nd commit updated functions

---
+++ src/main/java/pl/wojtek/demo/controllers/BlogController.java
@@ -0,0 +1,33 @@
+package pl.wojtek.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import pl.wojtek.demo.entities.Post;
+import pl.wojtek.demo.service.PostService;
+
+import java.util.List;
+
+@RestController
+public class BlogController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping(value = "/post")
+    public List<Post> post() {
+        return postService.getAllPosts();
+    }
+
+    @PostMapping(value = "/post")
+    public void publishPost(@RequestBody Post post) {
+        postService.insert(post);
+    }
+}
